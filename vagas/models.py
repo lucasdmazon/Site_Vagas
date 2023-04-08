da1b5024cd5170e3b109daaa721145103e021b1b
Lucas Daniel Mazon <77250387+lucasdmazon@users.noreply.github.com>
2023-04-08 13:31:57 -0300

    Criado os Models (Banco de dados)

--- vagas/models.py
+++ vagas/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
+
+
+class Vaga(models.Model):
+    titulo = models.CharField(max_length=255)
+    nome_empresa = models.CharField(max_length=255)
+    descricao_simplificada = models.CharField(max_length=255)
+    telefone = models.CharField(max_length=255, blank=True)
+    email = models.CharField(max_length=255, blank=True)
+    link = models.CharField(max_length=255)
+    data_publicacao = models.DateTimeField(default=timezone.now)
+    descricao = models.TextField(blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    curtir = models.BooleanField(default=False)
+
